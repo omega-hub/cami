78cc354e420eb54a425bcbf9a7862703108c1b29
DrDoak <larryzhao740@yahoo.com>
2016-05-02 15:49:14 -0500

    Added updated movieplayer. Playing/pause and loop works

--- apps/movieViewer.py
+++ apps/movieViewer.py
@@ -1,29 +1,71 @@
-if(isMaster()):
-    from oav import *
+# if(isMaster()):
+#     from oav import *
+#     print "Reached MovieViewer.py 2"
+#     uim = UiModule.createAndInitialize()
 
-    uim = UiModule.createAndInitialize()
+#     v = VideoStream()
 
-    v = VideoStream()
+#     v.open('/opt/data/Videos/4ktest.mp4')
 
-    v.open('/opt/data/Videos/4ktest.mp4')
+#     img = Image.create(uim.getUi())
+#     img.setData(v.getPixels())
+#     #img.setAutosize(False) # Inserting these resulted in it not working
+#     #img.setSize(uim.getUi().getSize())
+#     #img.setStereo(True)
+#     v.play()
 
-    img = Image.create(uim.getUi())
-    img.setData(v.getPixels())
-    img.setAutosize(False)
-    img.setSize(uim.getUi().getSize())
-    #img.setStereo(True)
-    v.play()
+    # def setLooping(loop):
+    #     v.setLooping(loop)
 
-    def setLooping(loop):
-        v.setLooping(loop)
+    # def setPlaying(loop):
+    #     v.setPlaying(loop)
 
-    def setPlaying(loop):
-        v.setPlaying(loop)
+    # def seekToTime(time):
+    #     place = v.getVideoLength()
+    #     place = place - place*(100-time)/100
+    #     v.seekToTime(place)
 
-    def seekToTime(time):
-        place = v.getVideoLength()
-        place = place - place*(100-time)/100
-        v.seekToTime(place)
+    # def restart():
+    #     v.seekToTime(0)
+from oav import *
 
-    def restart():
-        v.seekToTime(0)
+# Utility function to send data to the web client
+def calljs(methodname, data):
+    mc = getMissionControlClient()
+    if(mc != None):
+        mc.postCommand('@server::calljs ' + methodname + ' ' + str(data))
+
+uim = UiModule.createAndInitialize()
+
+v = VideoStream()
+
+v.open('/software/omegalib/release/modules/oav/example/small.mp4')
+#v.open('/opt/data/Videos/4ktest.mp4')
+img = Image.create(uim.getUi())
+img.setData(v.getPixels())
+looping = False
+v.play()
+
+def setLooping(loop):
+    print "Accepted Command:"
+    print loop
+    print "Before:"
+    print v.isLooping()
+    v.setLooping(loop)
+    print "After:"
+    print v.isLooping()
+
+def setPlaying(loop):
+    print "Accepted Command:"
+    print loop
+    print "Before:"
+    print v.isPlaying()
+    v.setPlaying(loop)
+    print "After:"
+    print v.isPlaying()
+
+def seekToTime(time):
+    v.seekToTime(time)
+
+def restart():
+    v.seekToTime(0)
