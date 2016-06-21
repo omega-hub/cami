a44c79ed33f60501f3d95a6108d3930ead5576f0
DrDoak <larryzhao740@yahoo.com>
2016-06-21 12:54:46 -0500

    Finished interface, fixed few control issues, Added adjustable parameters

--- apps/gameTest.py
+++ apps/gameTest.py
@@ -2,7 +2,6 @@ from omega import *
 from cyclops import *
 import os, glob,math, time, random
 
-print "Initializing Camera 2"
 # camera = getDefaultCamera()
 # camera.setPosition(0,0,0)
 # camera.setBackgroundColor(Color('blue'))
@@ -16,7 +15,7 @@ l1.setColor(Color(1, 1, 0.8, 1))
 l1.setAmbient(Color(0,0,0.1,1))
 l1.setLightType(LightType.Spot)
 l1.setLightDirection(Vector3(0, 0, -1))
-random.seed("pinata")
+random.seed("qwerty")
 # testObj = BoxShape.create(100, 100, 100)
 # testObj.getMaterial().setColor(redColor, whiteColor)
 # testObj.setPosition(Vector3(0, 0, 4))
@@ -27,36 +26,53 @@ random.seed("pinata")
 # Apply an emissive textured effect (no lighting)
 #box.setEffect("textured -v emissive -d cyclops/test/omega-transparent.png")
 
-screenWidth = 140
+screenWidth = 130
 screenHeight = 100
 lastFrameTime = 0
 entities = []
-
+currentPlayer = False
+score = 0
 #Adjustable Variables:
 maxAsteroids = 4.0
 numAsteroids = 0
-asteroidSpawnRate = 5
+asteroidSpawnRate = 20
+asteroidCurrPeriod = 0
 asteroidSpeed = 0.5
 
-maxSaucers = 2
+maxSaucers = 2.0
 numSaucers = 0
-saucerSpawnRate = 1
+saucerSpawnRate = 10
+saucerCurrPeriod = 0
+
 saucerSpeed = 0.4
 
-playerShotSpeed = 1
-playerFireRate = 5
-enemyShotFrequency = 0
+enemyFireFrequency = 0
 enemyAccuracy = 1.0
-currentPlayer = False
 enemyMaxShots = 3
-shotLifetime = 170
+shotLifetime = 50
 shotDamage = 10
+playerShotDamage = 100
+
+playerHealth = 10000
+playerMaxSpeed = 0.8
+playerShotSpeed = 1.5
+playerFireRate = 10
+playerMaxShots = 20
+playerThrustVal = 0.05
+
+
+def calljs(methodname, data):
+    mc = getMissionControlClient()
+    if(mc != None):
+        mc.postCommand('@server::calljs ' + methodname + ' ' + str(data))
 
 def initGame():
     print "Initializing Game"
     screenWidth = 100
     screenHeight = 100
     lastFrameTime = 0
+    global score
+    score = 0
     entities = []
 
     #Adjustable Variables:
@@ -65,14 +81,15 @@ def initGame():
     asteroidSpawnRate = 5
     asteroidSpeed = 4
 
-    maxSaucers = 2
+    maxSaucers = 2.0
     numSaucers = 0
     saucerSpawnRate = 1
     saucerSpeed = 4
     global currentPlayer
     currentPlayer = Player()
     createObj(currentPlayer,screenWidth/2,screenHeight/2)
-    return False
+    dummy = False
+    calljs('onPythoninit',dummy)
 
 def gameLoop(t,dt):
     global lastFrameTime
@@ -81,6 +98,15 @@ def gameLoop(t,dt):
     if (change > (1.0/60.0)):
         lastFrameTime = t
         gameTick(dt)
+
+def requestStatusUpdate():
+    global currentPlayer
+    if currentPlayer:
+        global score, currentPlayer
+        info = []
+        info.append(score)
+        info.append(currentPlayer.health)
+        calljs('updateHealthScore',info)
 
 def gameTick(dt):
     for ent in entities:   
@@ -94,15 +120,18 @@ def draw():
         ent.draw()
 
 def calculateSpawn():
+    global asteroidCurrPeriod, saucerCurrPeriod
+    asteroidCurrPeriod -= 1
+    saucerCurrPeriod -= 1
     if (numAsteroids + 1.0 <= maxAsteroids):
-        if ((random.random() * 1000) >= asteroidSpawnRate):
+        if (asteroidSpawnRate != 0 and asteroidCurrPeriod <= 0):
             spawnAsteroid()
     if (numSaucers + 1.0 <= maxSaucers):
-        if ((random.random() * 1000) >= saucerSpawnRate):
+        if (saucerSpawnRate != 0 and saucerCurrPeriod <= 0):
             spawnSaucer()
 
 def spawnAsteroid():
-    global numAsteroids
+    global numAsteroids, asteroidCurrPeriod, asteroidSpawnRate
     x = 0
     y = 0
     wallSide = random.random()
@@ -122,6 +151,8 @@ def spawnAsteroid():
     numAsteroids = numAsteroids + 1
     createObj(asteroid,x,y)
     asteroid.setAsteroidSize(1.0)
+    if asteroidSpawnRate != 0:
+        asteroidCurrPeriod = (600 - (asteroidSpawnRate * 6))
 
 
 def spawnSaucer():
@@ -141,9 +172,11 @@ def spawnSaucer():
     elif wallSide > 0.75:
         y = screenHeight
         x = random.randint(0,screenWidth)
-    numSaucers = numAsteroids + 1
+    numSaucers = numSaucers + 1
     saucer = Saucer()
     createObj(saucer,x,y)
+    if saucerSpawnRate != 0:
+        saucerCurrPeriod = (600 - (saucerSpawnRate * 6))
 
 def createObj(obj, x, y,args=[]):
     entities.append(obj)
@@ -151,12 +184,8 @@ def createObj(obj, x, y,args=[]):
     obj.setPosition(x,y)
 
 def destroyObj(obj):
-    print "destroying Object"
     obj.onDestroy()
-    print "done with onDestroy"
     entities.remove(obj)
-    print "Removed Object?"
-
 
 def calculateCollisions(obj):
     collideList = []
@@ -169,6 +198,7 @@ def calculateDistance(obj1,obj2):
     dist = 0.0
     dist = math.sqrt(math.pow(obj1.y-obj2.y,2) + math.pow(obj1.x-obj2.x,2))
     return dist
+
 def gameOver():
     print "GameOver"
 
@@ -186,9 +216,10 @@ class Entity:
         self.accX = 0
         self.accY = 0
         self.angle = 0
-        self.maxVelX = 10
-        self.maxVelY = 10
+        self.maxVelX = playerMaxSpeed
+        self.maxVelY = playerMaxSpeed
         self.damage = 30
+        self.isFighter = False
         return False
 
     def onDestroy(self):
@@ -201,6 +232,8 @@ class Entity:
         return False
 
     def setAngle(self,angle):
+        self.model.resetOrientation()
+        self.model.roll(angle)
         self.angle = angle
         
     def setVelocity(self,x,y):
@@ -251,37 +284,42 @@ class Fighter(Entity):
         self.health = self.maxHealth
         self.numShots = 4
         self.currShots = 0
-        self.fireRate = 10
-        self.currFireRate = 0
-        self.thrustVal = 1.0
+        self.currFirePeriod = 0
+        self.thrustVal = playerThrustVal
         self.thrusting = False
-        self.invincibleTime = 100
+        self.invincibleTime = 0
         self.isFighter = True
         return False
 
     def tick(self,dt):
         Entity.tick(self,dt)
         if (self.thrusting):
-            self.thurst()
+            self.thrust()
         else:
             self.resetThrust()
-        self.currFireRate = self.currFireRate - 1
+        self.currFirePeriod = self.currFirePeriod - 1
         self.invincibleTime = self.invincibleTime - 1
+        self.thrusting = False
 
     def thrust(self):
         self.accX = self.thrustVal * math.cos(self.angle)
         self.accY = self.thrustVal * math.sin(self.angle)
+        self.thrusting = True
 
     def resetThrust(self):
         self.accX = 0
         self.accY = 0
 
     def fire(self,fireAng =False):
+        # print "Firing shot"
 
-        if (self.currShots < self.maxShots and self.currFireRate <= 0):
+        if (self.currShots < self.maxShots and self.currFirePeriod <= 0):
             self.currShots = self.currShots + 1
-            print "Successful Shot fired"
-            self.currFireRate = self.fireRate
+            # print "Successful Shot fired"
+            if self == currentPlayer:
+                self.currFirePeriod = (22 - playerFireRate)
+            else:
+                self.currFirePeriod = 10
             newShot = Shot()
             # print "enemy firing shot"
             createObj(newShot,self.x,self.y,[self])
@@ -293,7 +331,10 @@ class Fighter(Entity):
 
     def onHit(self,damage):
         if self.invincibleTime <= 0:
-            self.invincibleTime = 60
+            if self == currentPlayer:
+                self.invincibleTime = 60
+            else:
+                self.invincibleTime = 20
             self.health = self.health - damage
             if self.health < 0:
                 destroyObj(self)
@@ -307,11 +348,18 @@ class Player(Fighter):
 
     def onCreate(self,x,y):
         Fighter.onCreate(self,x,y)
-        self.model = BoxShape.create(1.0,1.0,1.0)
-        self.radius = 0.2
-        self.health = 100
+        self.model = BoxShape.create(2.0,1.0,1.0)
+        self.radius = 0.5
+        self.maxHealth = playerHealth
+        self.health = self.maxHealth
+        self.maxShots = playerMaxShots
+        self.invincibleTime = 100
         return False
 
+    def tick(self,dt):
+        Fighter.tick(self,dt)
+        # self.thrust()
+        # self.fire()
     def onDestroy(self):
         gameOver()
         return False
@@ -321,16 +369,15 @@ class Player(Fighter):
 class Asteroid(Fighter):
     def onCreate(self,x,y):
         Fighter.onCreate(self,x,y)
-        print "Asteroid Created"
         self.health = 50
         self.damage = 50
-        self.invincibleTime = 0
+        self.invincibleTime = 10
         self.size = 1.0
         self.model = False
 
 
         angle = random.random()*math.pi
-        spd = random.uniform(asteroidSpeed/2,asteroidSpeed)
+        spd = random.uniform(asteroidSpeed/2.0,asteroidSpeed)
         newVelX = spd * math.cos(angle)
         newVelY = spd * math.cos(angle)
         self.setVelocity(newVelX,newVelY)
@@ -352,10 +399,9 @@ class Asteroid(Fighter):
                 break
         
     def onDestroy(self):
+        global numAsteroids
         Entity.onDestroy(self)
-        print "Asteroid Destroyed"
         if self.size > 0.25:
-            print "New Asteroids being created from debris"
             asteroid1 = Asteroid()
             createObj(asteroid1,self.x,self.y)
             asteroid1.setAsteroidSize(self.size/2)
@@ -363,6 +409,8 @@ class Asteroid(Fighter):
             asteroid2 = Asteroid()
             createObj(asteroid2,self.x,self.y)
             asteroid2.setAsteroidSize(self.size/2)
+        else:
+            numAsteroids = numAsteroids - 0.25
 
 ########Shot######################
 
@@ -370,14 +418,17 @@ class Shot(Entity):
 
     def onCreate(self,x,y,shooter):
         Entity.onCreate(self,x,y)
-        print "Shot Created"
         self.model = SphereShape.create(1,4)
 
         self.shooter = shooter
-        self.radius = 1
+        self.radius = 1.0
         self.totalShotTime = shotLifetime
         self.shotTime = self.totalShotTime
-        self.damage = shotDamage
+        if self.shooter == currentPlayer:
+            self.damage = playerShotDamage
+        else:
+            self.damage = shotDamage
+        print "shot initialized with damage: " , self.damage
         return False
 
     def setDamage(self,dmg):
@@ -390,8 +441,10 @@ class Shot(Entity):
         Entity.tick(self,dt)
         hitList = calculateCollisions(self)
         for obj in hitList:
-            if (obj != self.shooter and type(obj) is Fighter):
+            print "collision detected"
+            if (obj != self.shooter and obj.isFighter):
                 self.shotTime = 0
+                print "Shot hit with damage :" , self.damage
                 obj.onHit(self.damage)
                 break
         self.shotTime = self.shotTime - 1
@@ -399,7 +452,6 @@ class Shot(Entity):
             destroyObj(self)
 
     def onDestroy(self):
-        print "Shot is being destroyed"
         Entity.onDestroy(self)
         self.shooter.currShots = self.shooter.currShots - 1
 
@@ -425,7 +477,7 @@ class Saucer(Fighter):
     def tick(self,dt):
         Fighter.tick(self,dt)
         # print "saucer tick"
-        if ((random.random() * 1000) <= enemyShotFrequency):
+        if ((random.random() * 1000) <= enemyFireFrequency):
             # print self.y
             # print currentPlayer.y
             # print currentPlayer.y - self.y
@@ -433,12 +485,83 @@ class Saucer(Fighter):
             # print math.atan2(currentPlayer.y - self.y, currentPlayer.x - self.x)
             # print enemyAccuracy
             # print (random.random()* (1.0 - enemyAccuracy) - ((1.0-enemyAccuracy)/2))
-            targetAngle = math.atan2(currentPlayer.y - self.y, currentPlayer.x - self.x) + (random.random()* (1.0 - enemyAccuracy) - ((1.0-enemyAccuracy)/2))
+            targetAngle = math.atan2(currentPlayer.y - self.y, currentPlayer.x - self.x)
+            randomVal = random.random()
+            targetAngle += (randomVal* (1.0 - enemyAccuracy) - ((1.0-enemyAccuracy)/2))
             # print "Target ANgle: ", targetAngle
             self.fire(targetAngle)
+
+    def onDestroy(self):
+        global numSaucers
+        Entity.onDestroy(self)
+        numSaucers = numSaucers - 1.0
 
 initGame()
 
 def onUpdate(frame, t, dt):
     gameLoop(t,dt)
 setUpdateFunction(onUpdate)
+
+###############Game Settings###############
+
+def setMaxAsteroids(num):
+    global maxAsteroids
+    print "setting max asteroids to " , num
+    maxAsteroids  = num
+
+def setAsteroidSpawnRate(num):
+    global asteroidSpawnRate
+    print "setting asteroid Spawn Rate to " , num
+    asteroidSpawnRate = num
+
+def setAsteroidSpeed(num):
+    global asteroidSpeed
+    print "setting asteroid speed to " , num
+    asteroidSpeed = num
+
+def setMaxSaucers(num):
+    global maxSaucers
+    print "setting max Saucers to " , num
+    maxSaucers  = num
+
+def setSaucerSpawnRate(num):
+    global saucerSpawnRate
+    print "setting saucer spawn rate to " , num
+    saucerSpawnRate = num
+
+def setSaucerShotFrequency(num):
+    global enemyFireFrequency
+    print "setting enemy shot Frequency to " , num
+    enemyFireFrequency = num
+
+def setEnemyAccuracy(num):
+    global enemyAccuracy
+    print "setting enemy accuracy to " , num
+    enemyAccuracy = num
+
+def setPlayerMaxHealth(num):
+    global playerHealth, currentPlayer
+    print "setting player max health to " , num
+    playerHealth = num
+    currentPlayer.health = playerHealth
+
+def setPlayerSpeed(num):
+    global playerMaxSpeed,currentPlayer
+    print "setting player max speed to " , num
+    playerMaxSpeed = num
+    currentPlayer.maxVelX = playerMaxSpeed
+    currentPlayer.maxVelY = playerMaxSpeed
+
+def setPlayerThrustVal(num):
+    global playerThrustVal,currentPlayer
+    print "setting player thrust val to " , num
+    playerThrustVal = num
+    currentPlayer.thrustVal = playerThrustVal
+
+def setPlayerFireRate(num):
+    global playerFireRate
+    print "setting player firerate to " , num
+    playerFireRate = num
+
+def onReload():
+    return True
